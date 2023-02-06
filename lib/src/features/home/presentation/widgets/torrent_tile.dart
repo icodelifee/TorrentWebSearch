7cf3889566384ae125a9fef65eaae3af14f3d0b8
icodelifee <mithunsmenon@gmail.com>
2023-02-06 22:54:14 +0530

    feat: show click icon cursor when hovering tiles

--- lib/src/features/home/presentation/widgets/torrent_tile.dart
+++ lib/src/features/home/presentation/widgets/torrent_tile.dart
@@ -10,38 +10,52 @@ class TorrentTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-        onTap: () async {
-          await Clipboard.setData(ClipboardData(text: torrent.magnet!));
-          if (context.mounted) {
-            showSnackbar(context, 'Magnet copied to clipboard');
-          }
-        },
-        title: Text(torrent.title ?? ''),
-        subtitle: Row(
-          children: [
-            if (torrent.size != null) ...[
-              const Icon(Icons.folder),
-              const SizedBox(width: 4),
-              Text(torrent.size!),
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: Card(
+        child: ListTile(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          onTap: () async {
+            await Clipboard.setData(ClipboardData(text: torrent.magnet!));
+            if (context.mounted) {
+              showSnackbar(context, 'Magnet copied to clipboard');
+            }
+          },
+          title: Text(torrent.title ?? ''),
+          subtitle: Row(
+            children: [
+              if (torrent.size != null) ...[
+                const Icon(Icons.folder),
+                const SizedBox(width: 4),
+                Text(torrent.size!),
+              ],
+              if (torrent.seeds != null) ...[
+                const SizedBox(width: 8),
+                const Icon(Icons.arrow_upward),
+                const SizedBox(width: 4),
+                Text(torrent.seeds!),
+              ],
+              if (torrent.leechs != null) ...[
+                const SizedBox(width: 8),
+                const Icon(Icons.arrow_downward),
+                const SizedBox(width: 4),
+                Text(torrent.leechs!),
+              ],
+              if (torrent.added?.isNotEmpty ?? false) ...[
+                const SizedBox(width: 8),
+                const Icon(Icons.access_time),
+                const SizedBox(width: 4),
+                Flexible(
+                  child: Text(
+                    torrent.added!,
+                    maxLines: 1,
+                  ),
+                ),
+              ]
             ],
-            if (torrent.seeds != null) ...[
-              const SizedBox(width: 8),
-              const Icon(Icons.arrow_upward),
-              const SizedBox(width: 4),
-              Text(torrent.seeds!),
-            ],
-            if (torrent.leechs != null) ...[
-              const SizedBox(width: 8),
-              const Icon(Icons.arrow_downward),
-              const SizedBox(width: 4),
-              Text(torrent.leechs!),
-            ]
-          ],
+          ),
+          trailing: const Icon(Icons.link),
         ),
-        trailing: const Icon(Icons.link),
       ),
     );
   }
